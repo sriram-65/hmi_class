25f2fc9533011fae5ab069fcc574ea049d57e024
sriram <159551306+sriram-65@users.noreply.github.com>
2025-07-13 14:58:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask , session
 from routes.routes import home
-
+from datetime import timedelta
 app = Flask(__name__ , template_folder="templ")
 
 app.secret_key = "hmi_class"
+app.permanent_session_lifetime = timedelta(days=50)
 app.register_blueprint(home)
 
 
